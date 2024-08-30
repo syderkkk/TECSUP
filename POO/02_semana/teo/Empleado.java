771325dc4f82ef8eb855095bc8cc570670640190
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2024-08-29 22:03:43 -0500

    v1

---
+++ POO/02_semana/teo/Empleado.java
@@ -0,0 +1,20 @@
+package semana2.teo;
+
+public class Empleado {
+
+    String nombre;
+    String rango;
+    int dni;
+    double sueldo;
+
+    public Empleado(){
+
+    }
+
+    public Empleado(String nombre, String rango, int dni, double sueldo){
+        this.nombre = nombre;
+        this.rango = rango;
+        this.dni = dni;
+        this.sueldo = sueldo;
+    }
+}
---
+++ POO/02_semana/teo/Producto.java
@@ -0,0 +1,42 @@
+package semana2.teo;
+
+public class Producto {
+
+    private String codigo;
+    private double precio;
+    private String descripcion;
+
+    public Producto() {
+
+    }
+
+    public Producto(String codigo, double precio, String descripcion) {
+        this.codigo = codigo;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
