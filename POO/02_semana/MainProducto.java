77581821d3cd0ff071f6cc063d9b346bed8e02d1
Italo Mendoza <105523522+syderkkk@users.noreply.github.com>
2024-08-29 20:44:24 -0500

    v1

--- POO/02_semana/MainProducto.java
+++ POO/02_semana/MainProducto.java
@@ -3,7 +3,7 @@ package semana2;
 public class MainProducto {
 
     public static void main(String[] args) {
-        Producto producto = new Producto("001", "Laptop", 2, 1500);
+        Producto producto = new Producto("AB1", "Laptop", 2, 1500);
 
         System.out.println("Precio total: " + producto.calcularPrecioTotal());
         System.out.println("IGV: " + producto.calcularIGV());
